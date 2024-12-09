f9efb52c03ed73d6fc1d4009f0df1c4d690f05c2
Corentin Bringer <corentinbringer@ik.me>
2024-12-09 17:20:46 +0100

    spring cloud gateway

---
+++ src/test/java/fr/corentinbringer/gateway/GatewayApplicationTest.java
@@ -0,0 +1,13 @@
+package fr.corentinbringer.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTest {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
